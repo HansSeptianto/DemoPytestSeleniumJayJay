5d53992ae5e6b19dd938b6b859b2b49fa1eafa05
HansSeptianto <hans280901@gmai.com>
2024-07-29 11:37:48 +0700

    fix actions

--- helper/config.py
+++ helper/config.py
@@ -1,8 +1,10 @@
+import os
+
 web_url = "https://www.saucedemo.com/"
 web_username = "standard_user"
 web_username_locked = "locked_out_user"
 web_password = "secret_sauce"
-api_key_qase = "946632d03f02bd878b35c0295f08c39122d34abd18fd7d213b99492cecb4ebf1"
+api_key_qase = os.environ.get("API_KEY_QASE")
 host_qase = "https://api.qase.io/v1/result/"
 project_code_qase = "DPJS"
 test_run_qase = "1"
